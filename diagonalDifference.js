b984d0b7f906a90f6ecda71c945512cd4fcd1136
EdwinBanda <casemirobanda@gmail.com>
2023-12-15 20:25:18 +0200

    diagonalDifference

---
+++ diagonalDifference.js
@@ -0,0 +1,16 @@
+const diagonalDifference = (arr)=>{
+    let sumFirstDiag = 0
+    let sumSecDiag = 0
+    for(let i = 0; i < arr.length; i++){
+        sumFirstDiag += arr[i][i]
+        sumSecDiag += arr[i][arr.length -i -1]
+    }
+    return sumFirstDiag > sumSecDiag ? sumFirstDiag - sumSecDiag : sumSecDiag - sumFirstDiag
+}
+const matrix = [
+    [1,2,3]
+    ,[4,5,6]
+    ,[9,8,9]
+]
+const result = diagonalDifference(matrix)
+console.log(result)
