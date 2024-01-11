5483703acfbc421f754f79e9036ad1ff3a49e0d3
Edwin Banda <casemirobanda@gmail.com>
2024-01-11 09:57:44 +0200

    Pending: It works on my machine

--- gradingStudents.js
+++ gradingStudents.js
@@ -1,22 +1,57 @@
 const gradingStudents =  grades =>{
-    for(let i = 0; i < grades.length; i++){
-        if(grades[i] < 38){
-            console.log(grades[i])
+    const finalValues = []
+
+    for(let grade of grades){
+        if(grade < 38){
+            finalValues.push(grade)
         }else{
-            for(let j = 1; j < 3; j++){
-                if(grades[i] % 5 !== 0){
-                    grades[i]++
-                }
-            }
-            if(grades[i] % 5 !== 0){
-                console.log(grades[i] - 2)
+            let nextMultipleOfFive = Math.ceil(grade / 5) * 5
+            if(nextMultipleOfFive - grade < 3){
+                finalValues.push(nextMultipleOfFive)
             }else{
-                console.log(grades[i])
+                finalValues.push(grade)
             }
         }
+    }
+    for(let grade of finalValues){
+        console.log(grade)
     }
     
 }
 const marks = [73,67,38,33]
-console.log(gradingStudents(marks))
+const results = gradingStudents(marks)
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const gradingStudents =  grades =>{
+//     for(let i = 0; i < grades.length; i++){
+//         if(grades[i] < 38){
+//             console.log(grades[i])
+//         }else{
+//             for(let j = 1; j < 3; j++){
+//                 if(grades[i] % 5 !== 0){
+//                     grades[i]++
+//                 }
+//             }
+//             if(grades[i] % 5 !== 0){
+//                 console.log(grades[i] - 2)
+//             }else{
+//                 console.log(grades[i])
+//             }
+//         }
+//     }
+    
+// }
+// const marks = [73,67,38,33]
+// console.log(gradingStudents(marks))
 
